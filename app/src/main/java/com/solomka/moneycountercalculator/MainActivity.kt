919dcf03fe01edcba5c2aa46b208986a545697a8
AbixSol <ssolomka@gmail.com>
2020-10-14 14:37:07 +0200

    update some var and vals

--- app/src/main/java/com/solomka/moneycountercalculator/MainActivity.kt
+++ app/src/main/java/com/solomka/moneycountercalculator/MainActivity.kt
@@ -4,27 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.text.toInt as textToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editText200 = findViewById<EditText>(R.id.editText200)
-        val editText100 = findViewById<EditText>(R.id.editText100)
+        val editText200  = findViewById<EditText>(R.id.editText200) as EditText
+
+        //val num200 = editText200.text.toString().textToInt()
+
+        var editText100 : Int  = (editText100.text.toString()).textToInt()
+        var num100 :String = (editText100 * 100).toString()
+
         val editText50 = findViewById<EditText>(R.id.editText50)
         val editText20 = findViewById<EditText>(R.id.editText20)
         val editText10 = findViewById<EditText>(R.id.editText10)
         val editText5 = findViewById<EditText>(R.id.editText5)
         val editText2 = findViewById<EditText>(R.id.editText2)
         val editText1 = findViewById<EditText>(R.id.editText1)
-        val editText0_5 = findViewById<EditText>(R.id.editText0_5)
-        val editText0_2 = findViewById<EditText>(R.id.editText0_2)
-        val editText0_1 = findViewById<EditText>(R.id.editText0_1)
-        val editText0_05 = findViewById<EditText>(R.id.editText0_05)
-        val editText0_02 = findViewById<EditText>(R.id.editText0_02)
-        val editText0_01 = findViewById<EditText>(R.id.editText0_01)
+        val editText05 = findViewById<EditText>(R.id.editText0_5)
+        val editText02 = findViewById<EditText>(R.id.editText0_2)
+        val editText01 = findViewById<EditText>(R.id.editText0_1)
+        val editText005 = findViewById<EditText>(R.id.editText0_05)
+        val editText002 = findViewById<EditText>(R.id.editText0_02)
+        val editText001 = findViewById<EditText>(R.id.editText0_01)
 
-       // val amount200 = editText200.text.toString().toInt()*200
+        buttonTotal.setOnClickListener {
+            var totalMoney = num100
+            Toast.makeText(this, editText200.text, Toast.LENGTH_SHORT).show()
+            textViewTotal.text = totalMoney.toString()
+        }
     }
+
 }
+
+
