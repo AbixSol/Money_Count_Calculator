9112544f454984c31e0fcbe578a88f1494710539
AbixSol <ssolomka@gmail.com>
2020-10-19 10:36:29 +0200

    working logic updated thanks Anton :)

--- app/src/main/java/com/solomka/moneycountercalculator/MainActivity.kt
+++ app/src/main/java/com/solomka/moneycountercalculator/MainActivity.kt
@@ -18,20 +18,21 @@ class MainActivity : AppCompatActivity() {
     private fun sumClick(){
 
         buttonTotal.setOnClickListener(){
-            val mon200: Int = (editText200.text.toString()).textToInt() * 200
-            val mon100: Int = (editText100.text.toString()).textToInt() * 100
-            val mon50: Int = (editText50.text.toString()).textToInt() * 50
-            val mon20: Int = (editText20.text.toString()).textToInt() * 20
-            val mon10: Int = (editText10.text.toString()).textToInt() * 10
-            val mon5: Int = (editText10.text.toString()).textToInt() * 5
-            val mon2: Int = (editText2.text.toString()).textToInt() * 2
-            val mon1: Int = (editText1.text.toString()).textToInt() * 1
-            val mon05: Double = (editText0_5.text.toString()).textToInt() * 0.5
-            val mon02: Double = (editText0_2.text.toString()).textToInt() * 0.2
-            val mon01: Double = (editText0_1.text.toString()).textToInt() * 0.1
-            val mon005: Double = (editText0_05.text.toString()).textToInt() * 0.05
-            val mon002: Double = (editText0_02.text.toString()).textToInt() * 0.02
-            val mon001: Double = (editText0_01.text.toString()).textToInt() * 0.01
+            //val mon200: Int = (editText200.text.toString()).textToInt() * 200
+            val mon200: Int = if (editText200.text.toString().isBlank()) 0 else (editText200.text.toString()).textToInt() * 200
+            val mon100: Int = if (editText100.text.toString().isBlank()) 0 else(editText100.text.toString()).textToInt() * 100
+            val mon50: Int = if (editText50.text.toString().isBlank()) 0 else(editText50.text.toString()).textToInt() * 50
+            val mon20: Int = if (editText20.text.toString().isBlank()) 0 else(editText20.text.toString()).textToInt() * 20
+            val mon10: Int = if (editText10.text.toString().isBlank()) 0 else(editText10.text.toString()).textToInt() * 10
+            val mon5: Int = if (editText5.text.toString().isBlank()) 0 else(editText5.text.toString()).textToInt() * 5
+            val mon2: Int = if (editText2.text.toString().isBlank()) 0 else(editText2.text.toString()).textToInt() * 2
+            val mon1: Int = if (editText1.text.toString().isBlank()) 0 else(editText1.text.toString()).textToInt() * 1
+            val mon05: Double = if (editText0_5.text.toString().isBlank()) 0.0 else(editText0_5.text.toString()).textToInt() * 0.5
+            val mon02: Double = if (editText0_2.text.toString().isBlank()) 0.0 else(editText0_2.text.toString()).textToInt() * 0.2
+            val mon01: Double = if (editText0_1.text.toString().isBlank()) 0.0 else(editText0_1.text.toString()).textToInt() * 0.1
+            val mon005: Double = if (editText0_05.text.toString().isBlank()) 0.0 else(editText0_05.text.toString()).textToInt() * 0.05
+            val mon002: Double = if (editText0_02.text.toString().isBlank()) 0.0 else(editText0_02.text.toString()).textToInt() * 0.02
+            val mon001: Double = if (editText0_01.text.toString().isBlank()) 0.0 else(editText0_01.text.toString()).textToInt() * 0.01
             val total =(mon200 + mon100 + mon50 + mon20 + mon10 + mon5 +
                     mon2 + mon1 + mon05 + mon02 + mon01 + mon005 +
                     mon002 + mon001).toString()
