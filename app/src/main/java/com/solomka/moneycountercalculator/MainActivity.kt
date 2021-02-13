aed82110b01603fc63d1b2ef1ed6b94ced798a79
AbixSol <ssolomka@gmail.com>
2021-02-13 16:55:33 +0100

    Add splash screen and some new pictures all working.

--- app/src/main/java/com/solomka/moneycountercalculator/MainActivity.kt
+++ app/src/main/java/com/solomka/moneycountercalculator/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.DecimalFormat
+import kotlin.concurrent.thread
 import kotlin.text.toInt as textToInt
 
 class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         sumClick()
